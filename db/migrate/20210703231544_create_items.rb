610804caedf84e8da314f3883c35ea00e416445a
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-03 18:24:29 -0500

    add columns to items table and necessary associations

--- db/migrate/20210703231544_create_items.rb
+++ db/migrate/20210703231544_create_items.rb
@@ -1,6 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.integer :user_id
 
       t.timestamps null: false
     end
