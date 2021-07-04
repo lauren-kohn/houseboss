6ee8cfd944f5217ae5318835f20b29b0a1e8c44a
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-03 22:03:07 -0500

    gives post /login route ability to find user from params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-    
+        #find the user
+        @user = User.find_by(email: params[:email])
+        #authenticate the user
+        #log in
+        #redirect to the user's landing page
     end 
 
     get '/signup' do
