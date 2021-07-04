2d9cfe77cedad54eea125faffe138e3bedb16000
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-04 17:09:53 -0500

    add logged_in? and current_user helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    set :session_secret, "pickles_and_milo"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do 
+
+    def logged_in?
+      !!current_user
+    end 
+
+    def current_user 
+      #returns a user (if there is one) and nil if there isn't
+      #saves the current user as a variable to prevent unnecessary db calls (after the first one)
+      @current_user ||= User.find_by(id: session[:user_id])
+    end 
+
   end
 
 end
