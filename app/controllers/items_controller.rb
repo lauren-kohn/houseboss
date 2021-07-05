4f5fbead3317996337d5a5cb15888d090eaa5e94
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-05 14:18:42 -0500

    add post and show routes for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,22 @@ class ItemsController < ApplicationController
 
     #post item (allows form response; creates new item with params)
     post '/items' do 
-    
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:name] != "" && params[:description] != ""
+            @item = Item.create(name: params[:name], description: params[:description], user_id: current_user.id)
+            redirect "/items/#{@item.id}"
+        else
+            redirect "/items/new"
+        end
     end
 
     #show route for item
-
+    get '/items/:id' do 
+        @item = Item.find(params[:id])
+        erb :'/items/show'
+    end
 
     #index route for all items
 
