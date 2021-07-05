15381d2989053c8d20d68b31ac72d1f30e067701
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-05 18:30:46 -0500

    makes user's items available only to current user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,8 @@ class ItemsController < ApplicationController
 
     #index route for all items
     get '/items' do 
-        @items = Item.all
+        @items = current_user.items
+        #@items = Item.all - only suitable for if a user needs to see other users' items; not desireable at this time
         erb :'/items/index'
     end
 
@@ -33,14 +34,30 @@ class ItemsController < ApplicationController
     #this route renders the form to edit an item
     get '/items/:id/edit' do 
         set_item
-        erb :'/items/edit'
+        if logged_in?
+            if @item.user == current_user
+                erb :'/items/edit'
+            else
+                redirect "/users/#{current_user.id}"
+            end
+        else
+            redirect '/' 
+        end
     end
 
     #this route will edit a particular item
     patch '/items/:id' do 
         set_item
-        @item.update(name: params[:name], description: params[:description])
-        redirect "/items/#{@item.id}"
+        if logged_in?
+            if @item.user == current_user
+                @item.update(name: params[:name], description: params[:description])
+                redirect "/items/#{@item.id}"
+            else
+                redirect "/users/#{current_user.id}"
+            end 
+        else
+            redirect '/'
+        end
     end
 
     private
